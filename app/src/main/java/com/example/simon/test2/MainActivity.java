f8d2352f67a1d676984f871caf5e73aeb09a4a5b
Simon827 <simon.glassimo@gmail.com>
2016-10-12 17:24:21 +0200

    Test
    Test

---
+++ app/src/main/java/com/example/simon/test2/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.simon.test2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actions, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_new:
+                createAddDialog();
+                break;
+        }
+        return super.onOptionsItemSelected(item); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
